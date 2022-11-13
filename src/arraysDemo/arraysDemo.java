b0847d1eb9a47f75ee7549ca4bea8d4d701bc61a
acercagri <cagri-acer@outlook.com>
2022-11-13 13:04:55 +0300

    arraysDemo

---
+++ src/arraysDemo/arraysDemo.java
@@ -0,0 +1,16 @@
+package arraysDemo;
+
+public class arraysDemo {
+
+	public static void main(String[] args) {
+		String[] ogrenciler = new String[4];
+		ogrenciler[0]="mehmet";
+		ogrenciler[1]="ahmet";
+		ogrenciler[2]="nazli";
+		ogrenciler[3]="su";
+for(int i=0;i<ogrenciler.length;i++) {
+	System.out.println(ogrenciler[i]);
+}
+	}
+
+}
